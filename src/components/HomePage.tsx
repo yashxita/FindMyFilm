175102aed277dbc745037a901623b3f97fb6d14c
yashxita <gouriyashita@gmail.com>
2024-12-24 01:37:21 +0530

    feat: font added

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen ">
+      <h1 className=" font-knewave text-6xl">Hey</h1>
+    </div>
+  );
+};
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        knewave: ['Knewave', 'sans-serif'], 
+      },
     },
   },
   plugins: [],
